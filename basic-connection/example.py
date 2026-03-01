7df6d3d08804cab36d74af75ca20bdd60298d935
pegramay <alexyoungsoopegram@gmail.com>
2026-02-28 22:18:55 -0500

    connected to LM studio able to recieve and send responses

--- basic-connection/example.py
+++ basic-connection/example.py
@@ -5,14 +5,18 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 client = OpenAI(base_url=os.getenv('LLM_URL'), api_key="lm-studio")
 
+while True:
+    x = input("ask me: ")
+    print("=================================================================================")
+    if x == "quit" or x =='q':
+        break
+    completion = client.chat.completions.create(
+      model = os.getenv('LLM_MODEL'),
+      messages=[
+        {"role": "system", "content": "Always refer to me as 'sir fuego the wise' and every answer to a prompt must greet me."},
+        {"role": "user", "content": x}
+      ],
+      temperature=0.7, #Lower numbers more predictable, higher gives more creativity
+      reasoning_effort="high",) 
+    print(completion.choices[0].message.content)
 
-completion = client.chat.completions.create(
-  model = os.getenv('LLM_MODEL'),
-  messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
-  ],
-  temperature=0.7,
-)
-
-print(completion.choices[0].message)
