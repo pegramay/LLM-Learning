8b56540ee6a7a51c771eaabddab0922979679b9c
pegramay <alexyoungsoopegram@gmail.com>
2026-03-23 19:54:03 -0400

    Photo too big but can convert to bytes

--- vision/sauron.py
+++ vision/sauron.py
@@ -28,6 +28,9 @@ The purpose of converting it into base 64 is to get around file limitations on f
 
 Trade off of base64 though is that it increases the file size by around 33%
 """
+
+#Load bytes into image as byte array how do I open binary file to be into a byte array 
+#Need to know the mime type need to know what byte array is
 def img_to_base64(image_path):
     try: 
         # Construct the full path to the image.  This is crucial.
@@ -35,9 +38,12 @@ def img_to_base64(image_path):
         print(full_path)
 
         # Uses PIL to open image file
-        img = Image.open(full_path)
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='JPEG') #depends on type this is hardcoded to JPEG
+        img = Image.open(full_path) #<===== Doesn't really need to be PIL image
+        if img:
+            print("Pil opened the image file")
+        else:
+            print("PIL didnt work")
+        img.save("monkey.jpg", format="JPEG") #build new path with new file name <==== This is the problem
 
 
         """
@@ -51,8 +57,14 @@ def img_to_base64(image_path):
         Reads raw bytes (MAIN DIFF)
         """
         #Read content of BytesIO as bytes
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format="JPEG")
+        img_byte_arr.seek(0)
         img_bytes = img_byte_arr.read()
-
+        if img_bytes:
+            print("Contents read successfully")
+        else:
+            print("Contents failed to read")
         #base64.b64encode(): Encodes the binary data to Base64 format.
         #.decode('utf-8'): Converts the byte-like object into a readable string.
         base64_string = base64.b64encode(img_bytes).decode('utf-8')
